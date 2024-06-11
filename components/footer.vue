618cd9ed62c5af64f7cf5f1e01a0400baa89359f
Gene Sescon <sescongene.dev@gmail.com>
2024-06-11 11:07:25 +0800

    icons

--- components/footer.vue
+++ components/footer.vue
@@ -1,8 +1,21 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div>
-    Component: footer
+  <div class="max-w-screen-xl mx-auto my-4">
+    <div class="text-center font-bold text-2xl my-5">
+      <span>Social <span class="text-orange-500">Links</span></span>
+    </div>
+    <div class="flex justify-center">
+      <a href="https://www.linkedin.com/in/gene-sescon/"
+        ><Icon name="devicon:linkedin" class="m-5" size="30"
+      /></a>
+      <a href="https://web.facebook.com/gnscn/"
+        ><Icon name="devicon:facebook" class="m-5" size="30"
+      /></a>
+      <a href="https://github.com/sescongene"
+        ><Icon name="devicon:github-wordmark" class="m-5" size="30"
+      /></a>
+    </div>
   </div>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -4,11 +4,40 @@ import { useProjectStore } from "@/stores/project";
 const projectStore = useProjectStore();
 projectStore.fetch();
 const projects = computed(() => projectStore.list);
+const devIcons = [
+  "devicon:laravel-wordmark",
+  "devicon:amazonwebservices-wordmark",
+  "devicon:php",
+  "devicon:react-wordmark",
+  "devicon:vuejs-wordmark",
+  "devicon:nuxtjs-wordmark",
+  "devicon:typescript",
+  "devicon:javascript",
+  "devicon:tailwindcss",
+  "devicon:docker-wordmark",
+  "devicon:mysql-wordmark",
+  "devicon:nodejs-wordmark",
+  "devicon:wordpress"
+];
 </script>
 
 <template>
   <Banner />
   <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
-  <div class="text-center font-bold text-4xl my-10"><span>Recent <span class="text-orange-500">work</span></span></div>
+  <div class="text-center font-bold text-4xl my-10">
+    <span>Recent <span class="text-orange-500">work</span></span>
+  </div>
   <Entry v-for="project in projects" :project="project" />
+  <div class="text-center font-bold text-4xl my-10">
+    <span>Tech <span class="text-orange-500">Stack</span></span>
+  </div>
+  <div class="flex flex-wrap justify-center">
+    <Icon
+      v-for="(item, index) in devIcons"
+      :key="index"
+      :name="item"
+      class="m-5"
+      size="100"
+    />
+  </div>
 </template>
